cac3e77bcc0fea0058979fee9737471e1c4c9b92
김우리 <dlsltlvmfl1@gmail.com>
2023-07-18 19:55:10 +0900

    for merge

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import './App.css'
-import Router from './shared/Router'
+import './App.css';
+import Router from './shared/Router';
 
 function App() {
-  return <Router />
+  return <Router />;
 }
 
-export default App
+export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import styled, { createGlobalStyle, css } from 'styled-components';
+import Comments from '../components/Comments';
+import Comments2 from '../components/Comments2';
+import { useState } from 'react';
+
+const GlobalStyle = createGlobalStyle`
+*{  
+  box-sizing: border-box;
+}
+html {
+  font-size : 10px;
+}
+body {
+  width: 100%;
+  margin: 0;
+}
+`;
 
 const Home = () => {
-  return <div>Home</div>
-}
+  const [cold, setCold] = useState();
 
-export default Home
+  return (
+    <Wrapper className="App" cold={cold}>
+      <GlobalStyle />
+      <Comments setCold={setCold} />
+      home
+    </Wrapper>
+  );
+};
+
+export default Home;
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: burlywood;
+  ${(props) =>
+    props.cold &&
+    css`
+      background-color: skyblue;
+    `}
+`;
