ed665a5363083812a2e90ec321556e68eee39fd7
Jiwon Park <xoxojw.dev@gmail.com>
2023-07-19 00:03:23 +0900

    Chores: seoulApi.js 충돌 해결

--- src/axios/seoulApi.js
+++ src/axios/seoulApi.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 export const getReservations = async () => {
-  console.log('예약정보 get');
   const response = await axios(
     `http://openAPI.seoul.go.kr:8088/${process.env.REACT_APP_SEOUL_API_KEY}/json/ListPublicReservationSport/1/1000`
   );
--- src/components/Reservation.jsx
+++ src/components/Reservation.jsx
@@ -5,19 +5,16 @@ import { useSelector } from "react-redux";
 
 const Reservation = () => {
   const location = useSelector((state) => state.location);
+  console.log("store에서 불러온 위치 => ", location)
 
+  const maxPageItems = 5
   const [currentPage, setCurrentPage] = useState(1);
 
   const { isLoading, isFetching, data: reservations } = useQuery(
     "reservations",
-    getReservations,
-    {
-      staleTime: 60000,
-    }
+    ()=>getReservations(maxPageItems, currentPage),
   );
   const queryClient = useQueryClient();
-
-  if (isLoading || isFetching) return `로딩 중 입니다`;
 
   // 거리 계산 함수
   const calculateDistance = (lat1, lng1, lat2, lng2) => {
@@ -40,6 +37,34 @@ const Reservation = () => {
   const toRadians = (degrees) => {
     return degrees * (Math.PI / 180);
   };
+
+  // 현재 위치와 각 시설들과의 거리를 계산하여 정렬
+  useEffect(() => {
+    if (location && reservations) {
+      const nearByFacilities = reservations
+        .slice()
+        .sort((a, b) => {
+          const distanceA = calculateDistance(
+            location.lat,
+            location.lng,
+            a.X,
+            a.Y
+          );
+          const distanceB = calculateDistance(
+            location.lat,
+            location.lng,
+            b.X,
+            b.Y
+          );
+
+          return distanceA - distanceB;
+        });
+
+      // 업데이트된 nearByFacilities를 사용하거나 필요한 처리를 수행합니다.
+    }
+  }, [location, reservations]);
+
+  if (isLoading || isFetching) return `로딩 중 입니다`;
 
   // 현재 위치와 각 시설들과의 거리를 계산하여 정렬
   const nearByFacilities = reservations
