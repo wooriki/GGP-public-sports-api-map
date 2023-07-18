b6e01c5bb858262a070159b05460632463d36f25
Jiwon Park <xoxojw.dev@gmail.com>
2023-07-18 21:37:10 +0900

    Chore: fix typos

--- src/axios/seoulApi.js
+++ src/axios/seoulApi.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-export const getReservations = async (currentPage, maxPageItems) => {
+export const getReservations = async (maxPageItems, currentPage) => {
   console.log("예약정보 get")
   const startIndex = (currentPage - 1) * maxPageItems + 1;
   const endIndex = currentPage * maxPageItems;
-  const response = await axios(`http://openAPI.seoul.go.kr:8088/${process.env.REACT_APP_SEOUL_API_KEY}/json/ListPublicReservationSport/1/1000`);
+  const response = await axios(`http://openAPI.seoul.go.kr:8088/${process.env.REACT_APP_SEOUL_API_KEY}/json/ListPublicReservationSport/${startIndex}/${endIndex}`);
   return response.data.ListPublicReservationSport.row;
 };
