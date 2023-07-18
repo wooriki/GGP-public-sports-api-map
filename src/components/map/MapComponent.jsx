ba2410975b8d69b1ab49d43cc42b7515cf78c953
Kim Hwanhoon <kimhwanhoon@outlook.com>
2023-07-18 19:12:06 +0900

    temp

--- src/components/map/MapComponent.jsx
+++ src/components/map/MapComponent.jsx
@@ -8,6 +8,7 @@ const MapComponent = () => {
   useEffect(() => {
     const getData = async () => {
       const res = await publicAPI('/1/500/');
+      console.log(res);
       const geoCoords = `${res.data.ListPublicReservationSport.row[0].X},${res.data.ListPublicReservationSport.row[0].Y}`;
 
       const resp = await axios('http://localhost:3001', {
