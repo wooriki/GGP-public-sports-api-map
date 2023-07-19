6457f4d75bcd5a1ea261bc7db5e4388418a77e6d
sua <choisua98@gmail.com>
2023-07-19 20:10:18 +0900

    feat: 서울 api 소분류별(종목)가져오기 기능 구현

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
-import Search from '../components/Search';
 
 const Home = () => {
-  return (
-    <div>
-      <Search />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Home;
