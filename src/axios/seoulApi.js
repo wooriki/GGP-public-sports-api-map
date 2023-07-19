a3500500bc2c5b376d19a338e3f9873ec99d7fb9
Jiwon Park <xoxojw.dev@gmail.com>
2023-07-19 15:51:07 +0900

    Chores: 용도별 api 분리

--- src/axios/seoulApi.js
+++ src/axios/seoulApi.js
@@ -1,6 +1,13 @@
 import axios from 'axios';
 
-export const getReservations = async (maxPageItems, currentPage) => {
+export const getReservations = async () => {
+  const response = await axios(
+    `http://openAPI.seoul.go.kr:8088/${process.env.REACT_APP_SEOUL_API_KEY}/json/ListPublicReservationSport/1/1000`
+  );
+  return response.data.ListPublicReservationSport.row;
+};
+
+export const getFacilitiesForPagination = async (maxPageItems, currentPage) => {
   const startIndex = (currentPage - 1) * maxPageItems + 1;
   const endIndex = currentPage * maxPageItems;
   const response = await axios(
