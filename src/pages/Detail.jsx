ff95337e98d02e11e38cd412793a283ecc506c0a
조성록 <pigrok@joseonglog-ui-MacBookPro.local>
2023-07-19 18:45:55 +0900

    merge

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import Comments from '../components/comments/Comments';
-// import MapComponent from '../components/map/MapComponent';
 
 const Detail = () => {
   return (
     <div>
-      {/* <MapComponent /> */}
       <Comments />
     </div>
   );
